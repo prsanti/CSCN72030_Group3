3341222d6dec49b89f448c4410e6c76359e1ddc2
Paul Santiago <70616143+prsanti@users.noreply.github.com>
2024-11-12 21:03:44 -0500

    add create heartrate file functionality

--- FitScript/heartrate.h
+++ FitScript/heartrate.h
@@ -3,19 +3,62 @@
 // CSCN72030 - Project 3 - Fall24 - Group 3
 // heartrate signatures
 
+#define HEARTRATEFILE "heartrate.txt"
+#include <iostream>
+#include <fstream>
+using namespace std;
+#include "string.h"
+
 class Heartrate {
 public:
 	int bpm;
+	int restingBpm;
+	int activeBpm;
 public:
+	// constructor
+	Heartrate() {
+
+	}
+
+	// deconstructor
+	~Heartrate() {
+
+	}
+
+	bool createHeartrateFile() {
+		// create file
+		ofstream file(HEARTRATEFILE);
+
+		// if error return false
+		if (!file) {
+			cout << "Error creating file" << endl;
+			return false;
+		}
+
+		cout << "writing to file: " << HEARTRATEFILE << endl;
+		//file << 10 << endl;
+
+		return true;
+	}
+
 	// read heartrate from file
-	int readHeartrate();
+	int readBpm();
 
-	// display heartrate
-	int displayHeartrate();
+	// set bpm
+	bool setBpm();
 
-	// get average bpm
-	int getAverageBpm();
+	// calculate resting BPM
+	int calculateRestingBpm();
 
-	// write to file
-	void writeHeartrate();
+	// set resting bpm from calculation
+	bool setRestingBpm();
+
+	// calculate resting BPM
+	int calculateActiveBpm();
+
+	// set resting bpm from calculation
+	bool setActiveBpm();
+
+	// send bpm to HMI/SCADA
+	int sendBpmToHMI();
 };
